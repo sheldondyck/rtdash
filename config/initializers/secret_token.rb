c97042aba1b8932604042682c915adf8ca1137c4
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2011-11-22 09:48:11 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rtdash::Application.config.secret_token = '98f444d1f484ae5618988545f098e1abaa917de46b5469e5f902a961de4e1af2fb3710fe90ddf365075e8ae3b9d911f054b985cc5882f2b58515cec7c7486e57'
