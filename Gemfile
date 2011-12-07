d6326f4bc8f29f60c3cdc28833867cfbbf20d558
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2011-12-07 18:23:56 -0200

    added rspec tests for pages

--- Gemfile
+++ Gemfile
@@ -9,19 +9,24 @@ gem 'sqlite3'
 gem 'therubyracer'
 gem 'haml'
 gem 'faker'
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
-# Gems used only for assets and not required
-# in production environments by default.
+group :development do
+	gem 'rspec-rails'
+end
+
 group :assets do
   gem 'sass-rails',   '~> 3.1.5'
   gem 'coffee-rails', '~> 3.1.1'
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
-
-# To use ActiveModel has_secure_password
-gem 'bcrypt-ruby', '~> 3.0.0'
+group :test do
+  # Pretty printed test output
+  gem 'turn', '~> 0.8.3', :require => false
+	gem 'rspec'
+end
 
 # Use unicorn as the web server
 # gem 'unicorn'
@@ -32,7 +37,3 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
 
-group :test do
-  # Pretty printed test output
-  gem 'turn', '~> 0.8.3', :require => false
-end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'product'" do
+    it "should be successful" do
+      get 'product'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'price'" do
+    it "should be successful" do
+      get 'price'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'blog'" do
+    it "should be successful" do
+      get 'blog'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'press'" do
+    it "should be successful" do
+      get 'press'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'company'" do
+    it "should be successful" do
+      get 'company'
+      response.should be_success
+    end
+  end
+end
