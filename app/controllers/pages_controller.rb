ffdff36ea5d3643e1a7c9c14616e2e75f6006a9e
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2011-12-08 15:06:08 -0200

    extracted topbar css color to variables.css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,13 +5,13 @@ class PagesController < ApplicationController
   def home
     @title = 'Home'
     @latin = Faker::Lorem
-    $p_0 = @latin.sentence(4)
-    $sub_title_0 = @latin.words(3).join(' ').capitalize + '.' 
-    $sub_title_1 = @latin.words(3, true).join(' ').capitalize + '.' 
-    $sub_title_2 = @latin.words(3).join(' ').capitalize + '.' 
-    $sub_p_0 = @latin.paragraph(3)
-    $sub_p_1 = @latin.paragraph(3)
-    $sub_p_2 = @latin.paragraph(3)
+    @p_0 = @latin.sentence(4)
+    @sub_title_0 = @latin.words(3).join(' ').capitalize + '.' 
+    @sub_title_1 = @latin.words(3, true).join(' ').capitalize + '.' 
+    @sub_title_2 = @latin.words(3).join(' ').capitalize + '.' 
+    @sub_p_0 = @latin.paragraph(3)
+    @sub_p_1 = @latin.paragraph(3)
+    @sub_p_2 = @latin.paragraph(3)
   end
 
   def product
