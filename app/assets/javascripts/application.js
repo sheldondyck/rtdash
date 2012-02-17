19156b5255eba451dfb0eedf98936d6cab88c2ca
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2012-02-17 14:34:13 -0200

    bootstrap 2.0 upgrade

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,18 @@
 //= require jquery
 //= require jquery_ujs
 //
-//= require_tree './bootstrap'
+//= require 'bootstrap/bootstrap-tooltip.js'
+//= require 'bootstrap/bootstrap-popover.js'
+//= require 'bootstrap/bootstrap-scrollspy.js'
+//= require 'bootstrap/bootstrap-alert.js'
+//= require 'bootstrap/bootstrap-button.js'
+//= require 'bootstrap/bootstrap-carousel.js'
+//= require 'bootstrap/bootstrap-collapse.js'
+//= require 'bootstrap/bootstrap-dropdown.js'
+//= require 'bootstrap/bootstrap-modal.js'
+//= require 'bootstrap/bootstrap-tab.js'
+//= require 'bootstrap/bootstrap-transition.js'
+//= require 'bootstrap/bootstrap-typeahead.js'
 //
 //= require 'pages.js'
 //= require 'session.js'
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,9 +9,9 @@ describe "Users" do
       it "should not make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",         :with => ""
-          fill_in "Email",        :with => ""
-          fill_in "Password",     :with => ""
+          fill_in "user[name]",         :with => ""
+          fill_in "user[email]",        :with => ""
+          fill_in "user[password]",     :with => ""
           click_button
           response.should render_template('users/new')
           response.should have_selector('div.alert-message.error')
@@ -24,9 +24,9 @@ describe "Users" do
       it "should make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",         :with => "Example User"
-          fill_in "Email",        :with => "user@example.com"
-          fill_in "Password",     :with => "foobar"
+          fill_in "user[name]",         :with => "Example User"
+          fill_in "user[email]",        :with => "user@example.com"
+          fill_in "user[password]",     :with => "foobar"
           click_button
           response.should have_selector("div.alert-message.success")
           response.should render_template('users/show')
