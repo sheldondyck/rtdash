3da4e3c45ce219afff72a048ed469d36d9234df4
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2012-02-29 13:35:56 -0300

    added autorization checking

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,16 @@
 module SessionsHelper
-	
-	def sign_in(user)
+
+  def sign_in(user)
+    #cookies.delete(:remember_token)
+    logger.debug "SIGN_IN #{user.inspect}"
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     #logger.debug "SIGN_IN #{cookies.permanent.signed[:remember_token].inspect}"
-    #logger.debug "SIGN_IN #{user.inspect}"
     #logger.debug "SIGN_IN #{remember_token.inspect}"
     #logger.debug "SIGN_IN #{session.inspect}"
     #logger.debug "SIGN_IN #{cookies.inspect}"
 
     self.current_user = user
-	end
+  end
 
   def current_user=(user)
     @current_user = user
@@ -23,30 +24,44 @@ module SessionsHelper
     user == current_user
   end
 
-	def signed_in?
-		!current_user.nil?
-	end
+  def signed_in?
+    !current_user.nil?
+  end
 
-	def authenticate
-		deny_access unless signed_in?
-	end
-
-	def deny_access
-		redirect_to login_path, :notice => "Please sign in to access this page."
-	end
+  def authenticate
+    deny_access unless signed_in?
+  end
 
   def sign_out
     cookies.delete(:remember_token)
     self.current_user = nil
   end
 
-  private
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+  def deny_access
+    store_location
+    redirect_to login_path, :notice => "Please sign in to access this page."
+  end
 
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
-    end
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  private
+  def user_from_remember_token
+    User.authenticate_with_salt(*remember_token)
+  end
+
+  def remember_token
+    cookies.signed[:remember_token] || [nil, nil]
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
+  end
 end
 
